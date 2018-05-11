b31ffe35571f664bfd229850ba88d4f1b4b800c6
Youngsun Kim <yskim041@gmail.com>
2018-05-10 23:29:50 -0700

    auto download the pretrained model

--- scripts/get_state_dict.py
+++ scripts/get_state_dict.py
@@ -3,42 +3,75 @@
 Download pretrained ResNet50 params from:
   https://download.pytorch.org/models/resnet50-19c8e357.pth
 '''
-import math
+
 import torch
 import torch.nn as nn
-import torch.nn.init as init
 
+import math
+import os
 import sys
+sys.path.append('./')
 
-sys.path.append('../')
 from fpn import FPN50
 from retinanet import RetinaNet
 
 
-print('Loading pretrained ResNet50 model..')
-d = torch.load('../model/resnet50.pth')
+model_base_dir = './model'
+model_filename = os.path.join(model_base_dir, 'resnet50.pth')
+model_url = 'https://download.pytorch.org/models/resnet50-19c8e357.pth'
 
-print('Loading into FPN50..')
-fpn = FPN50()
-dd = fpn.state_dict()
-for k in d.keys():
-    if not k.startswith('fc'):  # skip fc layers
-        dd[k] = d[k]
+net_filename = os.path.join(model_base_dir, 'net.pth')
 
-print('Saving RetinaNet..')
-net = RetinaNet()
-for m in net.modules():
-    if isinstance(m, nn.Conv2d):
-        init.normal(m.weight, mean=0, std=0.01)
-        if m.bias is not None:
-            init.constant(m.bias, 0)
-    elif isinstance(m, nn.BatchNorm2d):
-        m.weight.data.fill_(1)
-        m.bias.data.zero_()
 
-pi = 0.01
-init.constant(net.cls_head[-1].bias, -math.log((1-pi)/pi))
+def load_pretrained_model():
+    if not os.path.isdir(model_base_dir):
+        os.makedirs(model_base_dir)
 
-net.fpn.load_state_dict(dd)
-torch.save(net.state_dict(), '../model/net.pth')
-print('Done!')
+    if not os.path.exists(model_filename):
+        print('Cannot find saved params: {}'.format(model_filename))
+        try:
+            import urllib.request as urll
+        except ImportError:
+            import urllib as urll
+
+        print('Downloading pretrained ResNet50 params from {}'.format(
+            model_url))
+        urll.urlretrieve(model_url, model_filename)
+        print('Saved in {}'.format(model_filename))
+
+    return torch.load(model_filename)
+
+
+def get_state_dict():
+    print('Loading pretrained ResNet50 model..')
+
+    d = load_pretrained_model()
+
+    print('Loading into FPN50..')
+    fpn = FPN50()
+    dd = fpn.state_dict()
+    for k in d.keys():
+        if not k.startswith('fc'):  # skip fc layers
+            dd[k] = d[k]
+
+    print('Saving RetinaNet..')
+    net = RetinaNet()
+    for m in net.modules():
+        if isinstance(m, nn.Conv2d):
+            nn.init.normal_(m.weight, mean=0, std=0.01)
+            if m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.BatchNorm2d):
+            m.weight.data.fill_(1)
+            m.bias.data.zero_()
+
+    pi = 0.01
+    nn.init.constant_(net.cls_head[-1].bias, -math.log((1-pi)/pi))
+
+    net.fpn.load_state_dict(dd)
+    torch.save(net.state_dict(), net_filename)
+    print('Done!')
+
+
+if __name__ == '__main__':
+    get_state_dict()
