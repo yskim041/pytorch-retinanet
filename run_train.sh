7de419b4fec83c18877c02c295e2e9584d5b8bf4
Youngsun Kim <yskim041@gmail.com>
2018-05-17 15:32:25 -0700

    update run_train.sh

--- run_train.sh
+++ run_train.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+if [ ! -f ./pretrained/net.pth ]; then
+    echo "Run 'init_retinanet.py' first by executing:"
+    echo ""
+    echo "    python init_retinanet.py"
+    echo ""
+    echo "and check if the net file is generated in pretrained/."
+    exit
+fi
+
 python3 train.py \
     --gpus "0" \
     --num_classes 49 \
     --train_batch_size 2 \
     --test_batch_size 1 \
+    --net "pretrained/food_net.pth" \
     --img_dir "datasets/food_all_images" \
     --train_list "datasets/food_ann_train.txt" \
     --test_list "datasets/food_ann_test.txt" \
