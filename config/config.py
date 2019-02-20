12a50409d966c5068b9e244aeb5c482801b04de2
Youngsun Kim <yskim041@gmail.com>
2019-02-20 14:10:39 -0800

    add sample_data & data augmentations

--- config/config.py
+++ config/config.py
@@ -6,7 +6,7 @@ import os
 gpu_id = '0'
 
 project_dir = os.path.split(os.getcwd())[0]
-project_prefix = 'can'
+project_prefix = 'sample'
 
 available_models = {
     'fpn50': {'model_name': 'fpn50', 'conv_layer': 'resnet50'},
@@ -17,19 +17,23 @@ model_key = 'fpn101'
 model_name = available_models[model_key]['model_name']
 base_conv_layer = available_models[model_key]['conv_layer']
 
-img_res = 500
+img_res = 600
 num_classes = 4
 
 train_batch_size = 2
 test_batch_size = 1
 
-dataset_dir = os.path.join(project_dir, 'data/can_data')
+dataset_dir = os.path.join(
+    project_dir, 'data/{}_data'.format(project_prefix))
 
-label_map_filename = os.path.join(dataset_dir, 'can_label_map.pbtxt')
+label_map_filename = os.path.join(
+    dataset_dir, '{}_label_map.pbtxt'.format(project_prefix))
 img_dir = os.path.join(dataset_dir, 'images')
 
-train_list_filename = os.path.join(dataset_dir, 'can_ann_train.txt')
-test_list_filename = os.path.join(dataset_dir, 'can_ann_test.txt')
+train_list_filename = os.path.join(
+    dataset_dir, '{}_ann_train.txt'.format(project_prefix))
+test_list_filename = os.path.join(
+    dataset_dir, '{}_ann_test.txt'.format(project_prefix))
 
 pretrained_dir = os.path.join(project_dir, 'pretrained')
 pretrained_filename = os.path.join(
